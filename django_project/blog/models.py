138c2769bdc2d2d7174f9dc5ced73c203879a341
govind35 <govind.kalsa35@gmail.com>
2020-06-16 02:30:56 +0530

    new post

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.utils import timezone #for time
 from django.contrib.auth.models import User #
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete= models.CASCADE )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):     # used to show the new post created
+        return reverse('post-detail', kwargs={'pk': self.pk}) # reverse will take to post-detail url with value of pk
+
+    
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse # for using Httpresponse
+from django.contrib.auth.mixins import LoginRequiredMixin   # class cant use loginRequired decorater so LoginRequiredMixin is used
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post # for getting data from Post database
 
 
 def home(request):
     return render(request, 'blog/home.html', {'posts': Post.objects.all()})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # naming covention is appname/model_viewtype.html for ListView but we can change it here
+    context_object_name = 'posts'       # to tell listview  which object to be passed which we used in html file 
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):       # naming covention is appname/model_viewtype.html for DetailView
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):       # naming covention is appname/model_viewtype.html for CreateView
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):     # to set logged in user for post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):       # naming covention is appname/model_viewtype.html for CreateView
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):     # to set logged in user for post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})   
